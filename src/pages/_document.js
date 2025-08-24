db54d825453874d44e659a82bd826b213d80f66f
IO1711 <52698195+IO1711@users.noreply.github.com>
2025-08-24 08:09:43 +0500

    google verification added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/svg+xml" href="/full_logo.svg" />
+        <meta name="google-site-verification" content="google-site-verification=e--hMoWnWpQcnuzV3R0tNW2Ie_sDQdS9uCR8OwaerGs" />
       </Head>
       <body>
         <Main />
